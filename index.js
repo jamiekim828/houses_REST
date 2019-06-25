a26b21d9edc4f6559a45fc1a3688874ae6b1028b
jamiekim828 <jamiekim828@gmail.com>
2019-06-25 17:37:38 +0200

    add start script in package.json

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const port = 4000
 const Sequelize = require('sequelize')
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
 const sequelize = new Sequelize('postgres://postgres:secret@localhost:5432/postgres',
     { define: { timestamps: false } })
 
